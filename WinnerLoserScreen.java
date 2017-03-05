f897811f9ca1be98fe1e65b30328845618723cbe
perezjquim <jquim1@hotmail.com>
2017-03-05 16:44:20 +0000

    gameover and images

--- WinnerLoserScreen.java
+++ WinnerLoserScreen.java
@@ -1,11 +1,4 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class WinnerLoserScreen here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class WinnerLoserScreen extends World
 {
 
