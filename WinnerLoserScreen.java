46c29c51d94f3d17e08326122ff09854396a8716
perezjquim <jquim1@hotmail.com>
2017-03-06 14:38:25 +0000

    gameover and soundfix(i hope)

--- WinnerLoserScreen.java
+++ WinnerLoserScreen.java
@@ -4,9 +4,9 @@ public class WinnerLoserScreen extends World
     public WinnerLoserScreen()
     {    
         super(600, 400, 1);
-        showWinner(false,"bosta1","bosta2");
+        showWinner(false,PimpMyCesto.P1Name,PimpMyCesto.P2Name);
         prepare();
-    }
+    }
     private void showWinner(boolean P1Won,String P1Name,String P2Name)
     {
         showText("WINNER:",300,100);
@@ -14,10 +14,11 @@ public class WinnerLoserScreen extends World
             showText(P1Name,300,130);
         else
             showText(P2Name,300,130);
-    }    
+    }    
+
     private void prepare()
     {
         addObject(new ExitButton(),300,325);
-        addObject(new ReturnButton(),300,225);
+        addObject(new ReturnButton(),300,200);
     }
 }
