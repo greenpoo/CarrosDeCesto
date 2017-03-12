b53a1e30e4baec9fdb30618fc2cda1e07b14e6dc
Paulo Quirino <quirinpa@gmail.com>
2017-03-12 16:54:28 +0000

    correcao

--- src/physics/PhysicsWorld.java
+++ src/physics/PhysicsWorld.java
@@ -40,7 +40,7 @@ public class PhysicsWorld extends World {
 		for (Map.Entry<Integer, PhysicsActor> entry : _actors.entrySet()) {
 			PhysicsActor actor = entry.getValue();
 
-			actor.setScale(_scale);// Faz scale da image do actor.
+			actor.setScale(_scale); // Diz ao actor qual é a scale do mundo.
 			actor.collideWithWalls(_size);// Faz a colição com as paredes
 			actor.simulateMovement(dt, dtDtO2);//Atualiza o vector do deslocamento, tendo em conta a equação do deslocamento.
 			actor.updateGFLocation(_scale);// Faz a atualização do actor tendo em conta o novo vector de deslocamento calculado por simulateMovement.
