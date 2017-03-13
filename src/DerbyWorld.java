862a7f2f09f4bd53703439293ba2056d44f2de59
perezjquim <jquim1@hotmail.com>
2017-03-13 23:47:25 +0000

    more comments and tweak to delay a little more after the game ends

--- src/DerbyWorld.java
+++ src/DerbyWorld.java
@@ -22,6 +22,11 @@ public class DerbyWorld extends World
         addObject(new Barrel(),(Greenfoot.getRandomNumber(10)+1)*55,(Greenfoot.getRandomNumber(10)+1)*35);
         addObject(new BoostPickup(),(Greenfoot.getRandomNumber(10)+1)*55,(Greenfoot.getRandomNumber(10)+1)*35);
     }
+    /**
+     * resetStats()
+     * 
+     * Método que faz 'reset' da vida e do 'boost' dos dois jogadores
+     */
     private void resetStats()
     {
         CarrosCesto.setP1Health(100);
@@ -29,6 +34,11 @@ public class DerbyWorld extends World
         CarrosCesto.setP1Boost(0);
         CarrosCesto.setP2Boost(0);
     }
+    /**
+     * spawnPlayers()
+     * 
+     * 'Faz nascer' os carros de cesto prontos a serem controlados no modo Derby
+     */
     private void spawnPlayers()
     {
         Player1 Player1 = new Player1();
@@ -40,21 +50,41 @@ public class DerbyWorld extends World
         Player2.getImage().mirrorVertically();
         Player2.turn(180);
     }
+    /**
+     * generateBarrels()
+     * 
+     * Método que gera inconstante e aleatoriamente barris pelo mundo DerbyWorld
+     */
     private void generateBarrels()
     {
         if(Greenfoot.getRandomNumber(500)<2)
             addObject(new Barrel(),(Greenfoot.getRandomNumber(10)+1)*55,(Greenfoot.getRandomNumber(10)+1)*35);
-    }    
+    }
+    /**
+     * generateBoostPickups()
+     * 
+     * Método que gera inconstante e aleatoriamente 'boosts' pelo mundo DerbyWorld
+     */
     private void generateBoostPickups()
     {
         if(Greenfoot.getRandomNumber(500)<2)
             addObject(new BoostPickup(),(Greenfoot.getRandomNumber(10)+1)*55,(Greenfoot.getRandomNumber(10)+1)*35);
     }
+    /**
+     * playBGM()
+     * 
+     * Reproduz a música de fundo do modo Derby
+     */
     private void playBGM()
     {
-        if(!GameModeMenu.getDerbySong().isPlaying())
+        if(!GameModeMenu.getDerbySong().isPlaying()) // Se ainda não tiver a ser reproduzida
             GameModeMenu.getDerbySong().playLoop();
     }
+    /**
+     * checkPlayer1Health()
+     * 
+     * Verifica a vida do Jogador 1 (e no caso de já estar a 0, acaba o jogo e o Jogador 2 é o vencedor)
+     */
     private void checkPlayer1Health()
     {
         if(CarrosCesto.getP1Health() <= 0)
@@ -62,10 +92,15 @@ public class DerbyWorld extends World
             GameModeMenu.getDerbySong().stop();
             GameModeMenu.setP1Won(false);            
             GameModeMenu.getFanfareSound().play();
-            Greenfoot.delay(60);
+            Greenfoot.delay(200);
             Greenfoot.setWorld(new WinnerLoserScreen());
         }
     }
+    /**
+     * checkPlayer2Health()
+     * 
+     * Verifica a vida do Jogador 2 (e no caso de já estar a 0, acaba o jogo e o Jogador 1 é o vencedor)
+     */
     private void checkPlayer2Health()
     {
         if(CarrosCesto.getP2Health() <= 0)
@@ -73,7 +108,7 @@ public class DerbyWorld extends World
            GameModeMenu.getDerbySong().stop();
            GameModeMenu.setP1Won(true);            
            GameModeMenu.getFanfareSound().play();
-           Greenfoot.delay(60);
+           Greenfoot.delay(200);
            Greenfoot.setWorld(new WinnerLoserScreen());
         }
     }
--- src/WinnerLoserScreen.java
+++ src/WinnerLoserScreen.java
@@ -1,4 +1,3 @@
-
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class WinnerLoserScreen extends World
 {
@@ -8,6 +7,11 @@ public class WinnerLoserScreen extends World
         showWinner(GameModeMenu.getP1Won(),PimpMyCesto.getP1Name(),PimpMyCesto.getP2Name());
         prepare();
     }
+    /**
+     * showWinner
+     * 
+     * Método que faz display do vencedor do jogo em questão
+     */
     private void showWinner(boolean P1Won,String P1Name,String P2Name)
     {
         showText("WINNER:",300,100);
@@ -22,12 +26,22 @@ public class WinnerLoserScreen extends World
         spawnExitButton();
         spawnReturnButton();
     }
+    /**
+     * spawnExitButton()
+     * 
+     * Desenha o botão 'EXIT' (que serve para parar a simulação/sair do jogo)
+     */
     private void spawnExitButton()
     {
         ExitButton ExitButton = new ExitButton();
         addObject(ExitButton,300,325);
         ExitButton.setImage("EXITBUTTON.png");
     }
+    /**
+     * spawnReturnButton()
+     * 
+     * Desenha o botão 'RETURN' (que serve para voltar ao menu principal do jogo - para voltar a jogar)
+     */
     private void spawnReturnButton()
     {
         ReturnButton ReturnButton = new ReturnButton();
