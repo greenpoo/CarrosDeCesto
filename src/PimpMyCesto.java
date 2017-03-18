6af7a88e48789b52da4f1d3b5cc0e19a96071f41
perezjquim <jquim1@hotmail.com>
2017-03-18 19:43:02 +0000

    solved nullpointerexception and added more color options

--- src/PimpMyCesto.java
+++ src/PimpMyCesto.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class PimpMyCesto extends World
 {
-    private Color CarColors[] = {Color.YELLOW,Color.RED,Color.GREEN,Color.BLUE,Color.PINK,Color.GRAY};
+    private Color CarColors[] = {Color.RED,Color.ORANGE,Color.YELLOW,Color.GREEN,Color.CYAN,Color.BLUE,Color.MAGENTA,Color.GRAY};
     private static GreenfootImage SelectedCarP1;
     private static GreenfootImage SelectedCarP2;
     private int CurrentImage;
@@ -23,21 +23,21 @@ public class PimpMyCesto extends World
     public void prepare()
     {
         setBackground("pimpmycesto.png");
-        spawnMenuCar();
+        prepareCar();
+        addObject(Car,300,250);
         spawnArrows();
         addObject(MainMenu.getMuteButton(),35,365);
         CurrentImage = 1;
-        PixelsGray = new double[Car.getImage().getWidth()][Car.getImage().getHeight()];
-        PixelsAlpha = new double[Car.getImage().getWidth()][Car.getImage().getHeight()];
-        Car_image = Car.getImage();
-        getPixels();
     }
-    private void spawnMenuCar()
+    private void prepareCar()
     {
         Car.setImage("images/carrocesto1_normal.png");
         Car.getImage().scale(150,112);
-        Car.setImage(tint(Car_image,CarColors[0]));
-        addObject(Car,300,250);
+        Car_image = Car.getImage();
+        PixelsGray = new double[Car_image.getWidth()][Car_image.getHeight()];
+        PixelsAlpha = new double[Car_image.getWidth()][Car_image.getHeight()];
+        getPixels();
+        Car.setImage(tint(Car.getImage(),CarColors[0]));
     }
     private void spawnArrows()
     {
@@ -69,10 +69,10 @@ public class PimpMyCesto extends World
     }
     private void hideIfLastCar()
     {
-        if(CurrentImage == 6 && ArrowR.getImage().getTransparency() == 255)
+        if(CurrentImage == 8 && ArrowR.getImage().getTransparency() == 255)
             ArrowR.getImage().setTransparency(0);
         else
-            if(CurrentImage != 6 && ArrowR.getImage().getTransparency() == 0)
+            if(CurrentImage != 8 && ArrowR.getImage().getTransparency() == 0)
                 ArrowR.getImage().setTransparency(255);
     }
     private void checkClickL()
@@ -96,7 +96,7 @@ public class PimpMyCesto extends World
         if((Greenfoot.mouseClicked(ArrowR) || Greenfoot.isKeyDown("d")) && !ArrowRClicked)
         {
             ArrowRClicked = true;
-            if(CurrentImage < 6)
+            if(CurrentImage < 8)
             {
                 CurrentImage++;
                 Car.setImage(tint(Car_image,CarColors[CurrentImage - 1]));
